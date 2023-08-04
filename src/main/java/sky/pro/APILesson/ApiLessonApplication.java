dbe40d3d3ee7aa2ae58ffb357475c021d2575ffc
kosmo <kosmosensei@gmail.com>
2023-08-04 13:38:26 +0700

    Initial commit

---
+++ src/main/java/sky/pro/APILesson/ApiLessonApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.APILesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/APILesson/ApiLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.APILesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
